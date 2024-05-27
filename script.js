33cb0df451ea271beb31972e57bf49f991c9a3ba
Harshuuuu2010 <harshdarji669@gmail.com>
2024-05-27 16:36:16 +0530

    Merge branch 'main' of https://github.com/Harshuuuu2010/Exam_form

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = encodeURIComponent(document.querySelector("#name").value);
+  const enrollment = encodeURIComponent(
+    document.querySelector("#enrollment").value
+  );
+  const batch = encodeURIComponent(document.querySelector("#batch").value);
+
+  window.location.href = `MCQ.html?name=${name}&enrollment=${enrollment}&batch=${batch}`;
+});
